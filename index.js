a329856156295fa775e01894847c5c61bcee8e0c
Marco Pincheira <marco.pincheira.s@gmail.com>
2017-09-08 15:31:27 -0300

    Implementacion de method caller

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
+const sha1 = require('sha1');
+const md5 = require('md5');
+
 let call = function(method, args) {
-  console.log(method, args);
+
+  let callback = null;
+
+  if ( !method.includes("_") ) {
+    throw "Formato de funcion inválido";
+  }
+
+  let data = {
+      method: method.replace('_', ':'),
+      user: null,
+      params: {},
+  };
+
+  data.signature = data.method;
+  data.signature += data.user;
+  data.signature += sha1( md5("wertyuhjc76091+**FS%&&/TBsadasdas") );
+
+  data.signature = sha1(data.signature);
+
+  if( args.length > 2 ) {
+    throw "Se admiten solo 2 argumentos para la función";
+  }
+
+  if ($args.length == 2) {
+    data.params = args[0];
+    callback = args[1];
+  } else {
+    data.params = ['dummy'];
+  }
+
+  let params = {
+    request: data,
+  };
+
+  let url = "http://api.buscalibre.local/local_ws.php/wsbknd";
+
+  request.post({url: url, form: params}, function (e, r, response) {
+
+    console.log(response);
+
+    if (typeof response !== "object") {
+      throw "Bad Response: " + response;
+    }
+
+    if (callback) {
+      callback(response.response);
+    }
+  });
 }
 
 let handler = {
-    get(target, propKey, receiver) {
-        return (...args) => call(propKey, args);
-    }
+  get(target, propKey, receiver) {
+    return (...args) => call(propKey, args);
+  }
 };
 
 module.exports = new Proxy({}, handler);
