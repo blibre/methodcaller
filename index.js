90647edfa759cc86e7cfe68d3a6168c3ad6c3475
Marco Pincheira <marco.pincheira.s@gmail.com>
2017-09-08 12:50:11 -0300

    implementando methodcaller como proxy

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = new Proxy({}, {
+  get : function(target, prop) { 
+    if(target[prop] === undefined) {
+      return function()  {
+        console.log('an otherwise undefined function!!');
+      };
+    } else {
+      return target[prop];
+    }
+  }
+});
