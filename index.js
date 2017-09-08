44652f792dca92db35501a7e5edf177ce37e9b98
Marco Pincheira <marco.pincheira.s@gmail.com>
2017-09-08 13:18:14 -0300

    logging

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 module.exports = new Proxy({}, {
-  get : function(target, prop) {
-    console.log(target, prop);
-    if(target[prop] === undefined) {
-      return function()  {
-        console.log('an otherwise undefined function!!');
-      };
-    } else {
-      return target[prop];
-    }
-  },
   apply: function(target, thisArg, args) {
     console.log(args);
   }
